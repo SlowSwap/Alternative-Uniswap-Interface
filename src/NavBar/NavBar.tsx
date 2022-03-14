0357a0053d418b6a307e603884a36550dfbca6e2
Justin Schuldt <justinschuldt@gmail.com>
2022-03-13 20:11:20 -0500

    show active route in nav

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 
 export default function NavBar() {
   const logoUrl = process.env.REACT_APP_SITE_URL + '/logo.svg'
+  const location = useLocation();
 
   return (
     <nav>
@@ -16,11 +17,14 @@ export default function NavBar() {
         <h2 className="mt-2 text-center text-5xl font-bold text-white">
           SlowSwap
         </h2>
-        <ul className="mt-2 mb-3 text-center text-xl text-gray-600 list-none flex items-center justify-center gap-8">
+        <ul className="mt-2 mb-3 text-center text-xl  list-none flex items-center justify-center gap-8">
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
-                <Link className="font-medium text-white" to={item.url}>
+                <Link
+                  className={"font-medium text-white " + (location.pathname === item.url ? "underline" : "")}
+                  to={item.url}
+                >
                   {item.title}
                 </Link>
               </li>
